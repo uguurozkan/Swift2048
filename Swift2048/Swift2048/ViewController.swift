311994513562769181c0d993adc9f2cc55621a16
student6 <student6@student6s-Mac-mini.local>
2014-12-23 14:10:57 +0200

    First commit

---
+++ Swift2048/Swift2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift2048
+//
+//  Created by student6 on 23/12/14.
+//  Copyright (c) 2014 Ugur Ozkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
